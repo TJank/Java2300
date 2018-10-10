9f47c6d00d7af4d36ee26c6253a2619773bf0e2d
Tyler <35941543+TJank@users.noreply.github.com>
2018-10-10 13:15:48 -0500

    Create and finish Programming assignment 3

---
+++ ProjectClass/src/Main.java
@@ -0,0 +1,50 @@
+/**
+ * Created By : Tyler Jankowski
+ * Date : 10-08-18
+ * Description : Creating and using a custom class for an RPG style game
+ *                  while using the custom class in the main.java class.
+ * Edited :
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] goods = {"Axe","Sword","Boots","Gauntlets","Shield","Spear"};
+        // Creating a new object of my RandomMerchant.java class using the constructor
+        RandomMerchant armory = new RandomMerchant(
+                "Armory",
+                10000,
+                goods);
+        // getting a view of the armory's inventory
+        System.out.println(armory.getInventory());
+        // using service method 1 to buy an item from the store
+        armory.buyFromMerchant("Axe", 200);
+        // using service method 2 to buy an item from the store, however
+        // this sore doesnt have sufficient funds
+        armory.sellToMerchant("Magical Sword", 15000);
+        // using setter with check for bad data
+        armory.setStoreMoney(-30000);
+        // showing resutls
+        System.out.println(armory.getStoreMoney());
+        // using setter to set new amount to merchant's total money
+        armory.setStoreMoney(20000);
+        // re-selling the overpriced sword
+        armory.sellToMerchant("Magical Sword", 15000);
+        // using getter to show merchant's money has been subtracted
+        System.out.println("The " + armory.getStoreName() + " has " + armory.getStoreMoney() + " funds left over.");
+        // using getter to show updated inventory
+        System.out.println(armory.getInventory());
+
+
+        // using default constructor to init a new object of RandomMerchant
+        RandomMerchant streetSide = new RandomMerchant();
+        // buying a item from the merchant
+        streetSide.buyFromMerchant("Health Potion", 50);
+        // selling an item to the merchant
+        streetSide.sellToMerchant("Shirt", 5);
+        // using the overridden toString method
+        System.out.println(streetSide.toString());
+
+    }
+}
---
+++ ProjectClass/src/RandomMerchant.java
@@ -0,0 +1,127 @@
+/**
+ * Created By : Tyler Jankowski
+ * Date : 10-08-18
+ * Description : Custom class for an RPG style game
+ *               while using the this class and methods in the main.java class.
+ * Edited :
+ */
+
+import java.util.Arrays;
+
+public class RandomMerchant {
+
+    private String storeName;
+    private int storeMoney;
+    private String[] inventory;
+
+    public RandomMerchant() {
+        this.storeName = "Street Vendor";
+        this.storeMoney = 1000;
+        this.inventory = new String[]{"Sword", "Axe", "Spear", "Chicken legs", "Veggie mix", "Health Potion", "Invisibility Potion"};
+    }
+
+    public RandomMerchant(String storeName, int storeMoney, String[] inventory) {
+        this.storeName = storeName;
+        this.storeMoney = storeMoney;
+        this.inventory = inventory;
+    }
+
+    // SERVICE METHOD 1
+    // @Param String item is the name of an item the user wants to sell
+    // @Param String price is the price of the item the user wants to sell
+    public void sellToMerchant(String item, int price) {
+        String[] tempArray = Arrays.copyOf(this.inventory, this.inventory.length);
+        if (price > this.storeMoney) {
+            System.out.println("Sorry your item is to expensive for this shop.");
+        } else {
+            this.storeMoney -= price;
+            this.inventory = Arrays.copyOf(tempArray, tempArray.length + 1);
+            this.inventory[this.inventory.length-1] = item;
+            System.out.println("The merchant handed you " + price + " gold in exchange for your " + item);
+        }
+    }
+
+    // SERVICE METHOD 2
+    // @Param String item is the name of an item the user wants to buy
+    // @Param String price is the price of the item the user wants to buy
+    public void buyFromMerchant(String item, int price) {
+        if(price > 0 && isInInventory(item)) {
+            int index = findIndex(item);
+            this.storeMoney += price;
+            this.inventory[index] = "SOLD";
+            System.out.println("You bought " + item + " for " + price + " gold coins.");
+        }
+    }
+
+    // SERVICE METHOD for buyFromMerchant
+    // @Param String item the name of the item the user wants to buy
+    private int findIndex(String item) {
+        int k = -1;
+        for(int i=0; i<this.inventory.length; i++) {
+            if(this.inventory[i].equalsIgnoreCase(item)) {
+                k = i;
+            }
+        }
+        return k;
+    }
+
+    // SERVICE METHOD for buyFromMerchant
+    // @Param String item the name of the item the user wants to buy
+    private boolean isInInventory(String item) {
+        boolean inInventory;
+        for(int i=0; i<this.inventory.length; i++) {
+            if(this.inventory[i].equalsIgnoreCase(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "RandomMerchant{" +
+                "storeName='" + storeName + '\'' +
+                ", storeMoney=" + storeMoney +
+                ", inventory=" + Arrays.toString(inventory) +
+                '}';
+    }
+
+    // @return returns the name of the store / merchant
+    public String getStoreName() {
+        return storeName;
+    }
+
+    // @Param String storeName is used to set a new name for the store
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    // @return returns the amount of money the store / merchant
+    public int getStoreMoney() {
+        return storeMoney;
+    }
+
+    // @Param int storeMoney used to set a new value to amount of money the store has
+    public void setStoreMoney(int storeMoney) {
+        if(storeMoney >=0) {
+            this.storeMoney = storeMoney;
+        }
+    }
+
+    // @return returns the inventory of the store / merchant
+    public String getInventory() {
+        String retrunStr = "";
+        for(int i =0; i<this.inventory.length; i++) {
+            retrunStr = retrunStr + " " + this.inventory[i];
+        }
+        return retrunStr;
+        // return Arrays.toString(this.inventory);
+    }
+
+    // @Param String[] inventory used to set a new inventory to store
+    public void setInventory(String[] inventory) {
+        if(inventory.length >1) {
+            this.inventory = inventory;
+        }
+    }
+}
